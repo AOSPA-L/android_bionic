spsforks/android-bionic-libc

libm: Optimize exp()
Perform fast exp(x) when |x| < 16.0.

Bug 1362653

Change-Id: I26060c75e7be63af169f00005fdb7cfe162c3a23
Reviewed-on: http://git-master/r/278360
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Johnny Qiu <joqiu@nvidia.com>
Tested-by: Johnny Qiu <joqiu@nvidia.com>
Reviewed-on: http://git-master/r/336759
Reviewed-by: Shu Zhang <chazhang@nvidia.com>
Tested-by: Shu Zhang <chazhang@nvidia.com>

libm/Android.mk
@@ -260,7 +260,9 @@ ifeq ($(TARGET_CPU_VARIANT),cortex-a9)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
+    arm/k_exp.S \
     arm/e_fast_pow.S \
+    arm/e_fast_exp.S \
     arm/e_sqrt.S \
     arm/e_sqrtf.S \
     arm/s_floor.S
@@ -271,7 +273,9 @@ ifeq ($(TARGET_CPU_VARIANT),cortex-a15)
 libm_arm_src_files += \
     arm/k_log2.S \
     arm/k_pow2.S \
+    arm/k_exp.S \
     arm/e_fast_pow.S \
+    arm/e_fast_exp.S \
     arm/e_sqrt.S \
     arm/e_sqrtf.S \
     arm/s_floor.S \

AOSPA-L/android_bionic

libm/Android.mk
@@ -294,7 +294,9 @@ LOCAL_SRC_FILES_arm += \
     arm/k_pow2.S \
     arm/e_fast_pow.S \
     arm/s_fast_sin.S \
-    arm/s_fast_cos.S
+    arm/s_fast_cos.S \
+    arm/e_fast_exp.S \
+    arm/k_exp.S
 
 endif
 