spsforks/android-bionic-libc

Optimization for sin/cos:     1. Add assembly version of sin/cos for arm     2. Enable a fast implementation of sin() and cos() for small     input (|x| < 640). It is only enabled for Cortex-A15.
Bug 1154547
Bug 1340861

Change-Id: I176cf84ea7df0e612f78d181b8324f31a231095e
Reviewed-on: http://git-master/r/277610
(cherry picked from commit 325d52623445ee089a0199fc993b28264b1cc9a6)

libm/Android.mk
@@ -274,7 +274,9 @@ libm_arm_src_files += \
     arm/e_fast_pow.S \
     arm/e_sqrt.S \
     arm/e_sqrtf.S \
-    arm/s_floor.S
+    arm/s_floor.S \
+    arm/s_fast_sin.S \
+    arm/s_fast_cos.S
 libm_arm_cflags += -DTARGET_CPU_VARIANT_CORTEX_A15
 libm_arm_asflags += -DTARGET_CPU_VARIANT_CORTEX_A15
 else

AOSPA-L/android_bionic

libm/Android.mk
@@ -292,7 +292,9 @@ LOCAL_SRC_FILES_arm += \
     arm/smaf.S
     arm/k_log2.S \
     arm/k_pow2.S \
-    arm/e_fast_pow.S
+    arm/e_fast_pow.S \
+    arm/s_fast_sin.S \
+    arm/s_fast_cos.S
 
 endif
 